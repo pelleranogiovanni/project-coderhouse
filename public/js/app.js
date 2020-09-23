c045ae59fb4dfaa400c2c3adb7c234c4a4ad9dac
pelleranogiovanni <pelleranogiovanni@hotmail.com>
2020-09-23 10:44:27 -0300

    refactoring function create card

--- public/js/app.js
+++ public/js/app.js
@@ -109,7 +109,7 @@ function calcularTotalPedido(productSelected){
 //funcion ejecutada al cargar el DOM, para cargar los prodcutos
 $(document).ready(() => {
 
-    let card = document.querySelector('#productContainer');
+    
 
     $.ajax({
         method: 'GET',
@@ -118,27 +118,7 @@ $(document).ready(() => {
         success: function(response) {
             products = response;
            
-            products.forEach(element => {
-
-                card.innerHTML += `
-                <div class="md:w-1/3 p-4 w-full my-6">
-                    <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105" src="${element.image}">
-                    </a>
-                    <div class="mt-4 text-left px-2">                      
-                    <h2 class="text-gray-800 title-font text-lg font-medium">${element.name}</h2>
-                    <div class="flex items-center justify-between">
-                        <p class="mt-1 text-gray-600">${element.price}</p>
-                        <button class="focus:outline-none btnProduct transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105" id="${element.id}">
-                            <svg aria-hidden="true" data-prefix="fas" data-icon="plus" class="hover:bg-indigo-700 svg-inline--fa fa-plus fa-w-14 w-8 h-8 text-white p-2 bg-indigo-500 rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>
-                        </button>                          
-                    </div>                      
-                    <p class="text-gray-500 text-xs tracking-widest title-font font-light">${element.description}</p>
-                    </div>
-                </div>                
-                `;
-            });
-            
+            domBuilder.createCard(products);            
         }
         
     });
